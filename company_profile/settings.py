b8a8ba1c0b6d8874be3db91a976442b33abf1857
Photic23 <resellcourtesyx@gmail.com>
2024-02-10 12:43:18 +0700

    rev requirments

--- company_profile/settings.py
+++ company_profile/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import environ # Tambahkan kode berikut
 import os
 import dj_database_url
+import django_heroku
 
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
