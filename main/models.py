0379e4fba213bd94aa9a5d075004f5d1d71facea
Photic23 <resellcourtesyx@gmail.com>
2024-02-20 11:17:13 +0700

    adding analytics

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class IpModel(models.Model):
+    ip = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ip
+    
+class PageViews(models.Model):
+    views = models.ManyToManyField(IpModel, related_name="post_views", blank=True)
+
+    def __str__(self):
+        return self.views
